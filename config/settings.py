bad72ccfc2c588626f55703c6479f9cff4a12a62
Myles Browne <myles@browne.link>
2021-10-02 14:48:25 +0100

    changes for dev push

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5wsbf(cv&bn573m94titktxsbwk0uxsk#llnwg(n2h!y!k^u+i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
